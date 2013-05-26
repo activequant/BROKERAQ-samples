4f268f95663d7cbc9102bb807fa98ffed921d3a0
activequant <ustaudinger@activequant.com>
2013-05-26 12:44:46 +0200

    Adding more code

---
+++ python/ex_feed.py
@@ -0,0 +1,16 @@
+from connectivity.messages_pb2 import *
+from connectivity.aq_socket import *
+from connectivity.message_listener import *
+from connectivity.definitions import *
+
+class MyListener(MessageListener):
+  def loggedin(self):
+    print "Logged in!"
+    
+
+
+aqsPrice = AqSocket(MyListener())
+aqsPrice.host = '192.168.0.6'
+aqsPrice.connect()
+aqsPrice.login("ustaudinger2", "abcd1234", "PRICE")
+aqsPrice.subscribe(Symbols.AUDCHF, "RAW")
---
+++ python/ex_login.py
@@ -0,0 +1,15 @@
+from connectivity.messages_pb2 import *
+from connectivity.aq_socket import *
+from connectivity.message_listener import *
+from connectivity.definitions import *
+
+class MyListener(MessageListener):
+  def loggedin(self):
+    print "Logged in!"
+    
+
+
+aqsPrice = AqSocket(MyListener())
+aqsPrice.host = '192.168.0.6'
+aqsPrice.connect()
+aqsPrice.login("ustaudinger2", "abcd1234", "PRICE")
