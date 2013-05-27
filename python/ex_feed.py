d75277945aefaae39cbecc47f9b78de215601756
activequant <ustaudinger@debian.(none)>
2013-05-27 13:37:18 -0400

    Adding latest ex_feed file.

--- python/ex_feed.py
+++ python/ex_feed.py
@@ -10,7 +10,17 @@ class MyListener(MessageListener):
 
 
 aqsPrice = AqSocket(MyListener())
+<<<<<<< HEAD
+aqsPrice.host = 'localhost'
+aqsPrice.connect()
+aqsPrice.login("ustaudinger2", "abcd1234", "PRICE")
+aqsPrice.subscribe(Symbols.GBPNOK, TimeFrames.RAW)
+aqsPrice.subscribe(Symbols.CHFNOK, TimeFrames.RAW)
+aqsPrice.subscribe(Symbols.SMICHF, TimeFrames.RAW)
+aqsPrice.subscribe(Symbols.DAXEUR, TimeFrames.RAW)
+=======
 aqsPrice.host = '192.168.0.6'
 aqsPrice.connect()
 aqsPrice.login("ustaudinger2", "abcd1234", "PRICE")
 aqsPrice.subscribe(Symbols.AUDCHF, TimeFrames.RAW)
+>>>>>>> d74cca68dcf47c125011f844bb6c14cda5a6f48d
