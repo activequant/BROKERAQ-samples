d6c7bc16d28925c893dc9ee596e5ba971b92de8a
activequant <ustaudinger@activequant.com>
2013-07-07 12:35:52 -0400

    Package reshuffling.

--- python/setup.py
+++ python/setup.py
@@ -2,8 +2,11 @@ from distutils.core import setup
 
 setup(
     name='BrokerAQ',
+    author='ActiveQuant GmbH',
+    author_email='info@activequant.com',
     version='0.1',
-    packages=['aq'],
+    url='http://www.brokeraq.com',
+    packages=['aq','aq.domainmodel','aq.stream','aq.util'],
     license='GPL3',
     long_description=open('README.txt').read(),
 )
